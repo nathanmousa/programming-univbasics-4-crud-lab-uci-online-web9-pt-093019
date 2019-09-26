97f07ab19aeb117ad66ccb551097cf5ff590f77a
Nathan Mousa <nathanmousa1@gmail.com>
2019-09-26 15:11:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = [1, 2, 3, 4]
+  element = array.push("arrays!")
 end
 
 def remove_element_from_end_of_array(array)
